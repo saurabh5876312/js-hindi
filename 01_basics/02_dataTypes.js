e15eccc79a7ed4645b436f49b829dbb91396dfe4
Saurabh Singh <saurabh88961@gmail.com>
2026-06-06 07:07:42 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+console.log(3 + 
+    3
+) //code readability shouid be high
+console.log("saurabh");
+let name = "saurabh"
+let age = 20
+let islogged = false
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => satandalone value
+//undefined => 
+//sumbol => unique
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
