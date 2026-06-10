4a5a1cd40fb9d1884d5f9f8b16ebc2b906cba31f
Saurabh Singh <saurabh88961@gmail.com>
2026-06-10 08:50:45 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,12 +21,63 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
+//let someNumber = 33
 
-let stringNumber = string(someNumber)
+//let stringNumber = string(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//************** operation*************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " saurabh"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
